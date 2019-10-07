bc3f25c8d6fda65cdad3ba9a6d1babfaa565ae19
hendersonigh <henderson.igh@gmail.com>
2019-10-07 17:35:14 -0400

    Enabled the opmode and changed the name to Encoder Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -51,8 +52,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
+@Autonomous(name="Encoder Drive", group="Linear Opmode")
 public class EncoderTest extends LinearOpMode {
     //DRIVE TRAIN MOTOR VARIABLES
     // Declare the motor variables
