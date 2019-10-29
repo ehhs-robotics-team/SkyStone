3c2d3a0fa7eb4ea32762ab26387fe39728f82aaa
Darrell Good (Nerdz Programming) <darzgood@gmail.com>
2019-10-29 16:24:30 -0400

    Added Parent TeleOP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -0,0 +1,141 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+/**
+ * Parent TeleOP class to hold driver controls.
+ */
+
+
+@TeleOp(name="Tele Parent", group ="Linear Opmode")
+@Disabled
+public abstract class TeleOP extends LinearOpMode {
+
+
+
+    //DRIVE TRAIN MOTOR VARIABLES
+    // Declare the motor variables
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor f_leftDrive = null;
+    private DcMotor f_rightDrive = null;
+    private DcMotor b_leftDrive = null;
+    private DcMotor b_rightDrive = null;
+
+    //Declare the variables for the claw servos
+    private Servo leftClaw = null;
+    private Servo rightClaw = null;
+
+    //Declare the gripper's servo variable
+    private CRServo gripperServo = null;
+
+
+
+
+    @Override public void runOpMode() {
+
+
+        //DRIVE TRAIN MOTOR SETUP
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+
+        //map the motors
+        f_leftDrive  = hardwareMap.get(DcMotor.class, "f_leftDrive");
+        f_rightDrive = hardwareMap.get(DcMotor.class, "f_rightDrive");
+        b_leftDrive = hardwareMap.get(DcMotor.class, "b_leftDrive");
+        b_rightDrive = hardwareMap.get(DcMotor.class, "b_rightDrive");
+
+        //map the claw servos
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+
+        //map the gripper's servo
+        gripperServo = hardwareMap.get(CRServo.class, "gripperServo");
+
+        //reverse the right motors
+        f_rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        b_rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //reverse one of the claw servos
+        rightClaw.setDirection(Servo.Direction.REVERSE);
+
+
+
+
+        //setup the values that are needed
+        double driveSensitivity = 1.5;
+        double clawUpPosition = 1.0;
+        double clawDownPosition = 0.55;
+
+
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //Run the child teleop run
+        main();
+
+    }
+
+
+    /*
+     * method for children autonomous opmodes to override and insert case specific moves.
+     */
+    public abstract void main();
+
+}
