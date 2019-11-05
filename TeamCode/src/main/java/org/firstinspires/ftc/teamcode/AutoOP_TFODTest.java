e7f3cbefede39cfd4d4eb4f1bce8d0fc0cb656a3
Darrell Good (Nerdz Programming) <darzgood@gmail.com>
2019-11-05 17:35:58 -0500

    Renamed auto tfod

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOP_TFODTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOP_TFODTest.java
@@ -29,13 +29,14 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@TeleOp(name="TFOD Test", group="Linear Opmode")
+@Autonomous(name="TFOD Test", group="Linear Opmode")
 
 public class AutoOP_TFODTest extends AutoOP {
 
